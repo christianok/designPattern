0a07b58258c0e82d7ed2a9b04ad60ca84bf12c37
zhuoli <zhuoli@dankegongyu.com>
2019-01-12 14:20:45 +0800

    解释器模式

---
+++ Interpreter/OrExpression.php
@@ -0,0 +1,18 @@
+<?php namespace design\interpreter;
+
+class OrExpression implements IExpression
+{
+    protected $exp1;
+    protected $exp2;
+
+    public function __construct($exp1, $exp2)
+    {
+        $this->exp1 = $exp1;
+        $this->exp2 = $exp2;
+    }
+
+    public function interpret($context)
+    {
+        return $this->exp1.interpret($context) || $this->exp2.interpret($context);
+    }
+}
