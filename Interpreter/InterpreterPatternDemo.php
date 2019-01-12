e418514b728d1f17b4fadefcc85dcf7dd4c07868
zhuoli <zhuoli@dankegongyu.com>
2019-01-12 14:42:58 +0800

    解释器模式

--- Interpreter/InterpreterPatternDemo.php
+++ Interpreter/InterpreterPatternDemo.php
@@ -2,5 +2,21 @@
 
 class InterpreterPatternDemo
 {
+    public static function getMaleExpression()
+    {
+        $robert = new TerminalExpression('Robert');
+        $john = new TerminalExpression('John');
 
+        return new OrExpression($robert, $john);
+    }
+
+    public static function getFemaleExpression()
+    {
+        $julie = new TerminalExpression('julie');
+        $lucy = new TerminalExpression('lucy');
+
+        return new AndExpression($julie, $lucy);
+    }
 }
+
+$demo = new InterpreterPatternDemo();
